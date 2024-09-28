1e568d2e1fdf2638ce11e623b758dcdf90525657
Dqniel <166188737+LDanielOchoa@users.noreply.github.com>
2024-09-28 17:57:42 -0500

    Update StartScreen.jsx

--- src/pages/StartScreen.jsx
+++ src/pages/StartScreen.jsx
@@ -1,4 +1,5 @@
 // src/pages/StartScreen.jsx
+
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import Background from '../components/Background';
@@ -12,9 +13,12 @@ const StartScreen = () => {
     <Background className="flex flex-col items-center justify-center min-h-screen p-4">
       <Logo />
       <h1 className="text-4xl font-bold mb-4">Bienvenido</h1>
-      <p className="text-lg mb-8">¡Hola! Cordial saludo a todos los auxiliares, lamentamos informarles
-      que la aplicacion se encuentra fuera de servicio hasta nuevo aviso, pedimos por favor realizar
-      su reporte interno por medio del grupo de whatsapp, lo sentimos mucho por las molestias. </p>
+      <p className="text-lg mb-8">
+        ¡Hola! Cordial saludo a todos los auxiliares, lamentamos informarles que la
+        aplicación se encuentra fuera de servicio hasta nuevo aviso. Pedimos por favor
+        realizar su reporte interno por medio del grupo de WhatsApp. Lo sentimos mucho
+        por las molestias.
+      </p>
     </Background>
   );
 };
